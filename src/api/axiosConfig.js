6d2468e9b7b195762eaa8b04a55672deb9f75c8f
harinderjit <harinderjit91218@gmail.com>
2023-08-16 11:24:29 +0530

    Server address changed

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://192.168.56.1:8080',
-    // baseURL: 'https://moviemania-api.onrender.com',
+    // baseURL: 'http://192.168.56.1:8080',
+    baseURL: 'https://moviemania-api.onrender.com',
     headers: {
         "ngrok-skip-browser-warning" : "any"
     }
